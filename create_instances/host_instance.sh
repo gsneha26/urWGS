bac115b3900b4a4a39d7bfa0c82245ef80065078
gsneha26 <gsneha@stanford.edu>
2021-06-19 06:28:25 +0000

    update simulation

--- create_instances/host_instance.sh
+++ create_instances/host_instance.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-gcloud compute instances create host-instance1 \
+gcloud compute instances create host-instance2 \
         --zone us-west1-a \
         --source-instance-template host-template \
-	--local-ssd=interface=NVME 
-#\
-#        --metadata startup-script='#!/bin/bash
+	--local-ssd=interface=NVME \
+        --metadata startup-script='#!/bin/bash
+		sudo apt-get --yes install git parallel'
 #		sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/nvme0n1
-#		sudo mkdir -p ^/^data
+#		sudo mkdir -p /data
 #		sudo mount -o discard,defaults /dev/nvme0n1 /data
 #		sudo chmod a+w /data'
