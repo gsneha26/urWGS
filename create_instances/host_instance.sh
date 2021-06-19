82ff07e70444f4bb7c7d804f266af69b8af06adb
gsneha26 <gsneha@stanford.edu>
2021-06-19 05:37:38 +0000

    updated startup script for host-instance

--- create_instances/host_instance.sh
+++ create_instances/host_instance.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-gcloud compute instances create $1 \
+gcloud compute instances create host-instance \
         --zone us-west1-a \
-        --source-instance-template annotation-template \
-	--create-disk=boot=yes,image=annotation-image-v5,size=100GB \
+        --source-instance-template host-template \
 	--local-ssd=interface=NVME \
         --metadata startup-script='#!/bin/bash
 		sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/nvme0n1
 		sudo mkdir -p /data
 		sudo mount -o discard,defaults /dev/nvme0n1 /data
-		sudo chmod a+w /data
-		gsutil cp gs://ultra_rapid_nicu/scripts/sample.config /data/
-		mkdir /data/scripts/
-		gsutil -m cp -r gs://ultra_rapid_nicu/scripts/annotation/* /data/scripts/
-		gsutil -o "GSUtil:parallel_thread_count=1" -o "GSUtil:sliced_object_download_max_components=8" cp gs://ur_wgs_public_data/GRCh37.fa /data/
-		chmod +x /data/scripts/*.sh
-		chmod a+w -R /data/
-		echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin\n*/1 * * * * bash -c /data/scripts/annotate_pmd_wrapper.sh >> /data/pmd_stdout.log 2>> /data/pmd_stderr.log\n*/1 * * * * bash -c /data/scripts/annotate_sniffles_wrapper.sh >> /data/sniffles_stdout.log 2>> /data/sniffles_stderr.log" | crontab -u gsneha -'
+		sudo chmod a+w /data'
