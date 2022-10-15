64ed1541d544bccd28733c387f69e9c4f4aa1162
gsneha26 <gsneha@stanford.edu>
2022-10-15 00:54:27 -0700

    updated cuda install version

--- setup/create_instance_images/guppy_mm2_image_setup.sh
+++ setup/create_instance_images/guppy_mm2_image_setup.sh
@@ -7,10 +7,11 @@ sudo apt install -y build-essential \
 
 git clone https://github.com/gsneha26/macos_setup.git
 
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.4.1/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.1-470.57.02-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.1-470.57.02-1_amd64.deb
-sudo apt-key add /var/cuda-repo-ubuntu2004-11-4-local/7fa2af80.pub
+wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
 sudo apt-get update
 sudo apt-get -y install cuda
 
