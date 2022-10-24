50b8bf99d4ade98376f36fc1721cdc03f540aed9
gsneha26 <gsneha@stanford.edu>
2022-10-24 16:58:28 -0700

    guppy-mm2 jobs every minute

--- manage_instances/guppy_mm2_instance.sh
+++ manage_instances/guppy_mm2_instance.sh
@@ -18,7 +18,7 @@ for z in $(gcloud compute accelerator-types list | grep nvidia-tesla-a100 | grep
   		bash -c ./urWGS/setup/mount_ssd_nvme.sh
   		mv urWGS /data/
   		export PROJECT_DIR=/data/urWGS
-      gsutil -o "GSUtil:parallel_thread_count=1" -o "GSUtil:sliced_object_download_max_components=8" cp gs://ur_wgs_test_data/GRCh37.mmi /data/
+        gsutil -o "GSUtil:parallel_thread_count=1" -o "GSUtil:sliced_object_download_max_components=8" cp gs://ur_wgs_test_data/GRCh37.mmi /data/
   		CONFIG_FILE_URL=$(gcloud compute instances describe $(hostname) --zone=$(gcloud compute instances list --filter="name=($(hostname))" --format "value(zone)") --format=value"(metadata[CONFIG_FILE_URL])")
   		gsutil cp $CONFIG_FILE_URL /data/sample.config
   		echo "2" > /data/postprocess_status.txt
@@ -26,8 +26,8 @@ for z in $(gcloud compute accelerator-types list | grep nvidia-tesla-a100 | grep
   		echo "2" > /data/upload_status.txt
   		chmod a+w -R /data/
   		chmod +x $PROJECT_DIR/*/*.sh
-      $PROJECT_DIR/guppy_mm2/generate_scripts.sh
-  		echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin\nPROJECT_DIR=$PROJECT_DIR\n2-59/3 * * * * bash -c $PROJECT_DIR/guppy_mm2/run_basecalling_wrapper.sh >> /data/logs/basecall_stdout.log 2>> /data/logs/basecall_stderr.log\n*/3 * * * * bash -c $PROJECT_DIR/guppy_mm2/run_alignment_wrapper.sh >> /data/logs/align_stdout.log 2>> /data/logs/align_stderr.log\n*/5 * * * * bash -c $PROJECT_DIR/guppy_mm2/upload_log.sh" | crontab -'
+        $PROJECT_DIR/guppy_mm2/generate_scripts.sh
+  		echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin\nPROJECT_DIR=$PROJECT_DIR\n* * * * * bash -c $PROJECT_DIR/guppy_mm2/run_basecalling_wrapper.sh >> /data/logs/basecall_stdout.log 2>> /data/logs/basecall_stderr.log\n* * * * * bash -c $PROJECT_DIR/guppy_mm2/run_alignment_wrapper.sh >> /data/logs/align_stdout.log 2>> /data/logs/align_stderr.log\n*/3 * * * * bash -c $PROJECT_DIR/guppy_mm2/upload_log.sh" | crontab -'
   
   EXIT_CODE=$?
   if [[ $EXIT_CODE -eq 0 ]]; then
