f4db0b565847f60d8cc6ff936f3e4975ee33a4dc
gsneha26 <gsneha@stanford.edu>
2023-01-09 03:49:40 +0530

    pmdv and sniffles update

--- manage_instances/guppy_mm2_instance.sh
+++ manage_instances/guppy_mm2_instance.sh
@@ -13,12 +13,12 @@ for z in $(gcloud compute accelerator-types list | grep nvidia-tesla-a100 | grep
   	--local-ssd=interface=NVME \
     --metadata FC=$2,CONFIG_FILE_URL=$3,startup-script='#!/bin/bash
   		nvidia-smi -pm 1
-        rm -rf urWGS
+      rm -rf urWGS
   		git clone https://github.com/gsneha26/urWGS.git -b phase2 
   		bash -c ./urWGS/setup/mount_ssd_nvme.sh
   		mv urWGS /data/
   		export PROJECT_DIR=/data/urWGS
-        gsutil -o "GSUtil:parallel_thread_count=1" -o "GSUtil:sliced_object_download_max_components=8" cp gs://ur_wgs_test_data/GRCh37.mmi /data/
+      gsutil -o "GSUtil:parallel_thread_count=1" -o "GSUtil:sliced_object_download_max_components=8" cp gs://ur_wgs_test_data/GRCh37.mmi /data/
   		CONFIG_FILE_URL=$(gcloud compute instances describe $(hostname) --zone=$(gcloud compute instances list --filter="name=($(hostname))" --format "value(zone)") --format=value"(metadata[CONFIG_FILE_URL])")
   		gsutil cp $CONFIG_FILE_URL /data/sample.config
   		echo "2" > /data/postprocess_status.txt
@@ -26,7 +26,7 @@ for z in $(gcloud compute accelerator-types list | grep nvidia-tesla-a100 | grep
   		echo "2" > /data/upload_status.txt
   		chmod a+w -R /data/
   		chmod +x $PROJECT_DIR/*/*.sh
-        $PROJECT_DIR/guppy_mm2/generate_scripts.sh
+      $PROJECT_DIR/guppy_mm2/generate_scripts.sh
   		echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin\nPROJECT_DIR=$PROJECT_DIR\n* * * * * bash -c $PROJECT_DIR/guppy_mm2/run_basecalling_wrapper.sh >> /data/logs/basecall_stdout.log 2>> /data/logs/basecall_stderr.log\n* * * * * bash -c $PROJECT_DIR/guppy_mm2/run_alignment_wrapper.sh >> /data/logs/align_stdout.log 2>> /data/logs/align_stderr.log\n*/3 * * * * bash -c $PROJECT_DIR/guppy_mm2/upload_log.sh" | crontab -'
   
   EXIT_CODE=$?
--- manage_instances/pmdv_instance.sh
+++ manage_instances/pmdv_instance.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-gcloud compute instances create $1 \
-    --zone us-west1-a \
+for z in $(gcloud compute machine-types list | grep n1-standard-96 | grep 'us-' | sort -V | awk '{print $2}'); do
+
+  echo $z
+  gcloud compute instances create $1 \
+    --zone $z \
     --machine-type n1-standard-96 \
-    --create-disk=boot=yes,image=pmd-image-v7,size=100GB \
+    --create-disk=boot=yes,image=variant-calling-image-ph2-v1,size=100GB,mode=rw,type=pd-balanced \
     --scopes=storage-full,compute-rw,logging-write \
     --local-ssd=interface=NVME \
     --metadata CHR=$2,CONFIG_FILE_URL=$3,startup-script='#!/bin/bash
-    git clone https://github.com/gsneha26/urWGS.git -b phase2 
-    bash -c ./urWGS/setup/mount_ssd_nvme.sh
-    mv urWGS /data/
-    export PROJECT_DIR=/data/urWGS
-    CONFIG_FILE_URL=$(gcloud compute instances describe $(hostname) --zone=$(gcloud compute instances list --filter="name=($(hostname))" --format "value(zone)") --format=value"(metadata[CONFIG_FILE_URL])")
-    gsutil cp $CONFIG_FILE_URL /data/sample.config
-    echo "2" > /data/pmdv_status.txt 
-    chmod a+w -R /data/
-    chmod +x $PROJECT_DIR/*/*.sh
-    echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin\nPROJECT_DIR=$PROJECT_DIR\n*/1 * * * * bash -c $PROJECT_DIR/pmdv/run_pmdv_pipeline_wrapper.sh >> /data/stdout.log 2>> /data/stderr.log" | crontab -'
+      rm -rf urWGS 
+      git clone https://github.com/gsneha26/urWGS.git -b phase2 
+      bash -c ./urWGS/setup/mount_ssd_nvme.sh
+      mv urWGS /data/
+      export PROJECT_DIR=/data/urWGS
+      CONFIG_FILE_URL=$(gcloud compute instances describe $(hostname) --zone=$(gcloud compute instances list --filter="name=($(hostname))" --format "value(zone)") --format=value"(metadata[CONFIG_FILE_URL])")
+      gsutil cp $CONFIG_FILE_URL /data/sample.config
+      echo "2" > /data/pmdv_status.txt 
+      chmod a+w -R /data/
+      chmod +x $PROJECT_DIR/*/*.sh
+      echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin\nPROJECT_DIR=$PROJECT_DIR\n*/1 * * * * bash -c $PROJECT_DIR/pmdv/run_pmdv_pipeline_wrapper.sh >> /data/stdout.log 2>> /data/stderr.log" | crontab -'
+
+  EXIT_CODE=$?
+  if [[ $EXIT_CODE -eq 0 ]]; then
+      break
+  fi
+done
