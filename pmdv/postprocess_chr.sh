b47dc4d3d4751a03d491c1786739f41019f5bf7e
gsneha26 <gsneha@stanford.edu>
2023-01-10 20:04:29 +0530

    updated postprocess

--- pmdv/postprocess_chr.sh
+++ pmdv/postprocess_chr.sh
@@ -10,13 +10,13 @@ if [ $? -gt 0 ]; then
 	exit 1
 fi
 
-gsutil -q -o "GSUtil:parallel_composite_upload_threshold=750M" -m cp $CHR_FOLDER/margin/MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam ${HP_BAM_BUCKET}/${SAMPLE}_$1.bam
+gsutil -q -o "GSUtil:parallel_composite_upload_threshold=750M" -m cp $CHR_FOLDER/intermediate_files/PHASED.PEPPER_MARGIN.haplotagged.bam ${HP_BAM_BUCKET}/${SAMPLE}_$1.bam
 if [ $? -gt 0 ]; then
 	1>&2 echo "Error with uploading haplotagged bam for chr"$1
 	exit 1
 fi
 
-gsutil -q -o "GSUtil:parallel_composite_upload_threshold=750M" -m cp $CHR_FOLDER/margin/MARGIN_PHASED.PEPPER_SNP_MARGIN.haplotagged.bam.bai ${HP_BAM_BUCKET}/${SAMPLE}_$1.bam.bai
+gsutil -q -o "GSUtil:parallel_composite_upload_threshold=750M" -m cp $CHR_FOLDER/intermediate_files/PHASED.PEPPER_MARGIN.haplotagged.bam.bai ${HP_BAM_BUCKET}/${SAMPLE}_$1.bam.bai
 if [ $? -gt 0 ]; then
 	1>&2 echo "Error with uploading haplotagged bam index file for chr"$1
 	exit 1
