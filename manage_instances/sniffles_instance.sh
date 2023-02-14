d55897657d49b0840f7acdd48858b2115302a16f
gsneha26 <gsneha@stanford.edu>
2023-02-14 13:35:05 -0800

    chr for startup script in sniffles

--- manage_instances/sniffles_instance.sh
+++ manage_instances/sniffles_instance.sh
@@ -10,6 +10,7 @@ for z in $(gcloud compute machine-types list | grep n1-standard-96 | grep 'us-' 
     --scopes=storage-full,compute-rw,logging-write \
     --local-ssd=interface=NVME \
     --metadata CHR=$2,CONFIG_FILE_URL=$3,startup-script='#!/bin/bash
+      set -e
       rm -rf urWGS 
       git clone https://github.com/gsneha26/urWGS.git -b phase2 
       bash -c ./urWGS/setup/mount_ssd_nvme.sh
@@ -18,7 +19,10 @@ for z in $(gcloud compute machine-types list | grep n1-standard-96 | grep 'us-' 
       export PROJECT_DIR=/data/urWGS
       CONFIG_FILE_URL=$(gcloud compute instances describe $(hostname) --zone=$(gcloud compute instances list --filter="name=($(hostname))" --format "value(zone)") --format=value"(metadata[CONFIG_FILE_URL])")
       gsutil cp $CONFIG_FILE_URL /data/sample.config
-      for i in $( echo $CHR | sed s/:/ /g ); do
+      mkdir -p /data/sniffles_status
+      CHR=$(gcloud compute instances describe $(hostname) --zone=$(gcloud compute instances list --filter="name=($(hostname))" --format "value(zone)") --format=value"(metadata[CHR])")
+      for i in $( echo $CHR | tr : " " ); do
+          echo "$i"
           echo "2" > /data/sniffles_status/${i}_sniffles_status.txt 
       done
       echo "2" > /data/sniffles_status.txt 
