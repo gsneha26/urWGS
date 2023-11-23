cc7658cca036f4a3a8d10ea36d80b1563cd7ebbb
gsneha26 <gsneha@stanford.edu>
2023-11-22 19:09:13 -0800

    simulation corrected for empty flowcell folder

--- simulation/simulate_flowcell.sh
+++ simulation/simulate_flowcell.sh
@@ -5,14 +5,16 @@ OUTPUT_FOLDER=$2/no_sample/*_${3}_*
 RUNTIME=$4
 
 NUM_POD5=$(find $INPUT_FOLDER/ -name '*.pod5' | wc -l)
-PERIOD=$(python3 -c "print (abs(int(${RUNTIME}/${NUM_POD5})))")
+if [ ${NUM_POD5} -gt 0 ]; then
+    PERIOD=$(python3 -c "print (abs(int(${RUNTIME}/${NUM_POD5})))")
 
-cd $INPUT_FOLDER
-echo $INPUT_FOLDER
+    cd $INPUT_FOLDER
+    echo $INPUT_FOLDER
 
-# copy one file at a time, separated by $PERIOD seconds
-for i in $(find . -name '*.pod5');
-do
+    # copy one file at a time, separated by $PERIOD seconds
+    for i in $(find . -name '*.pod5');
+    do
         cp --parents $i $OUTPUT_FOLDER/
         sleep ${PERIOD}s
-done
+    done
+fi
