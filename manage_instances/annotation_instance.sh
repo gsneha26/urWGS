5742faf8e9257dfc54afa9a189e28bc2ebb4478c
gsneha26 <gsneha26@stanford.edu>
2024-02-13 17:00:30 -0800

    annotation instance update

--- manage_instances/annotation_instance.sh
+++ manage_instances/annotation_instance.sh
@@ -5,13 +5,13 @@ for z in $(gcloud compute machine-types list | grep n1-standard-96 | grep 'us-' 
   echo $z
   gcloud compute instances create $1 \
     --zone $z \
-    --source-instance-template annotation-template \
-    --create-disk=boot=yes,image=annotation-image-v5,size=100GB \
+    --machine-type n1-standard-96 \
+    --create-disk=boot=yes,image=variant-calling-v1,size=200GB,mode=rw,type=pd-balanced \
     --scopes=storage-full,compute-rw,logging-write \
     --local-ssd=interface=NVME \
     --metadata CONFIG_FILE_URL=$2,startup-script='#!/bin/bash
       rm -rf urWGS 
-      git clone https://github.com/gsneha26/urWGS.git -b phase2 
+      git clone https://github.com/gsneha26/urWGS.git -b dorado_dev 
       bash -c ./urWGS/setup/mount_ssd_nvme.sh
       mv urWGS /data/
       gsutil -o "GSUtil:parallel_thread_count=1" -o "GSUtil:sliced_object_download_max_components=8" cp gs://ur_wgs_test_data/GRCh37.fa /data/
